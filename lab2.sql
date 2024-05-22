c75b14b4e76b3c7b0da20b0eb56292cb23bb4e64
Kulman-Denis <124732232+Kulman-Denis@users.noreply.github.com>
2024-05-22 10:58:14 +0300

    Add files via upload

---
+++ lab2.sql
@@ -0,0 +1,59 @@
+-- 1. Вивести значення наступних колонок: номер, код, новинка, назва, ціна, сторінки
+SELECT Numer, Cod, New, Name, Price, Sides FROM lab1.work;
+
+-- 2. Вивести значення всіх колонок
+SELECT * FROM lab1.work;
+
+-- 3. Вивести значення колонок в наступному порядку: код, назва, новинка, сторінки, ціна, номер
+SELECT Cod, Name, New, Sides, Price, Numer FROM lab1.work;
+
+-- 4. Вивести значення всіх колонок 10 перших рядків
+SELECT * FROM lab1.work LIMIT 10;
+
+-- 5. Вивести значення всіх колонок 10% перших рядків
+SELECT * FROM `lab1.work` HAVING RAND() > 0.9;
+
+-- 6. Вивести значення колонки код без повторення однакових кодів
+SELECT DISTINCT Cod FROM lab1.work;
+
+-- 7. Вивести всі книги новинки
+SELECT * FROM lab1.work WHERE New = 'Yes';
+
+-- 8. Вивести всі книги новинки з ціною від 20 до 30
+SELECT * FROM lab1.work WHERE New = 'Yes' AND Price BETWEEN 20 AND 30;
+
+-- 9. Вивести всі книги новинки з ціною менше 20 і більше 30
+SELECT * FROM lab1.work WHERE New = 'Yes' AND (Price < 20 OR Price > 30);
+
+-- 10. Вивести всі книги з кількістю сторінок від 300 до 400 і з ціною більше 20 і менше 30
+SELECT * FROM lab1.work WHERE (Sides >300 AND Sides< 400) AND (Price > 20 AND Price < 30);
+
+-- 11. Вивести всі книги видані взимку 2000 року
+SELECT * FROM lab1.work WHERE YEAR(Date) = 2000 AND MONTH(Date) IN (12, 1, 2);
+
+-- 12. Вивести книги зі значеннями кодів 5110, 5141, 4985, 4241
+SELECT * FROM lab1.work WHERE Cod IN (5110, 5141, 4985, 4241);
+
+-- 13. Вивести книги видані в 1999, 2001, 2003, 2005 р
+SELECT * FROM lab1.work WHERE YEAR(Date) IN (1999, 2001, 2003, 2005);
+
+-- 14. Вивести книги назви яких починаються на літери А-К
+SELECT * FROM lab1.work WHERE Name REGEXP '^[А-К]';
+
+-- 15. Вивести книги назви яких починаються на літери "АПП", видані в 2000 році з ціною до 20
+SELECT * FROM lab1.work WHERE Name LIKE 'Апп%' AND YEAR(Date) = 2000 AND Price < 20;
+
+-- 16. Вивести книги назви яких починаються на літери "АПП", закінчуються на "е", видані в першій половині 2000 року
+SELECT * FROM lab1.work WHERE Name LIKE 'Апп%е' AND Date BETWEEN '2000-01-01' AND '2000-06-30';
+
+-- 17. Вивести книги, в назвах яких є слово Microsoft, але немає слова Windows
+SELECT * FROM lab1.work WHERE Name LIKE '%Microsoft%' AND Name NOT LIKE '%Windows%';
+
+-- 18. Вивести книги, в назвах яких присутня як мінімум одна цифра
+SELECT * FROM lab1.work WHERE Name REGEXP '[0-9]';
+
+-- 19. Вивести книги, в назвах яких присутні не менше трьох цифр
+SELECT * FROM lab1.work WHERE Name REGEXP '([0-9].*){3}';
+
+-- 20. Вивести книги, в назвах яких присутній рівно п'ять цифр
+SELECT * FROM lab1.work WHERE Name REGEXP '([0-9].*){5}' AND Name NOT REGEXP '([0-9].*){6}';
---
+++ lab3.sql
@@ -0,0 +1,60 @@
+-- 1. Вивести книги у яких не введена ціна або ціна дорівнює 0
+SELECT * FROM lab1.work WHERE Price IS NULL OR Price = 0;
+
+-- 2. Вивести книги у яких введена ціна, але не введений тираж
+SELECT * FROM lab1.work WHERE Price IS NOT NULL AND Circulation IS NULL;
+
+-- 3. Вивести книги, про дату видання яких нічого не відомо.
+SELECT * FROM lab1.work WHERE Date IS NULL;
+
+-- 4. Вивести книги, з дня видання яких пройшло не більше року.
+SELECT * FROM lab1.work WHERE DATEDIFF(CURRENT_DATE, Date) <= 365;
+
+-- 5. Вивести список книг-новинок, відсортованих за зростанням ціни
+SELECT * FROM lab1.work WHERE New = 'Yes' ORDER BY Price ASC;
+
+-- 6. Вивести список книг з числом сторінок від 300 до 400, відсортованих в зворотному алфавітному порядку назв
+SELECT * FROM lab1.work WHERE Sides BETWEEN 300 AND 400 ORDER BY Name DESC;
+
+-- 7. Вивести список книг з ціною від 20 до 40, відсортованих за спаданням дати
+SELECT * FROM lab1.work WHERE Price BETWEEN 20 AND 40 ORDER BY Date DESC;
+
+-- 8. Вивести список книг, відсортованих в алфавітному порядку назв і ціною по спадаючій
+SELECT * FROM lab1.work ORDER BY Name ASC, Price DESC;
+
+-- 9. Вивести книги, у яких ціна однієї сторінки < 10 копійок.
+SELECT * FROM lab1.work WHERE (Price / Sides) < 0.10;
+
+-- 10. Вивести значення наступних колонок: число символів в назві, перші 20 символів назви великими літерами
+SELECT LENGTH(Name) AS Name_Length, UPPER(LEFT(Name, 20)) AS First_20_Capitalized FROM lab1.work;
+
+-- 11. Вивести значення наступних колонок: перші 10 і останні 10 символів назви прописними буквами, розділені '...'
+SELECT CONCAT(UPPER(LEFT(Name, 10)), '...', UPPER(RIGHT(Name, 10))) AS First_Last_10_Capitalized FROM lab1.work;
+
+-- 12. Вивести значення наступних колонок: назва, дата, день, місяць, рік
+SELECT Name, Date, DAY(Date) AS Day, MONTH(Date) AS Month, YEAR(Date) AS Year FROM lab1.work;
+
+-- 13. Вивести значення наступних колонок: назва, дата, дата в форматі 'dd / mm / yyyy'
+SELECT Name, Date, DATE_FORMAT(Date, '%d / %m / %Y') AS Formatted_Date FROM lab1.work;
+
+-- 14. Вивести значення наступних колонок: код, ціна, ціна в грн., ціна в євро, ціна в руб.
+SELECT Cod, Price, Price * 36.93 AS UAH, Price * 0.93 AS EUR, Price * 80.16 AS RUB FROM lab1.work;
+
+-- 15. Вивести значення наступних колонок: код, ціна, ціна в грн. без копійок, ціна без копійок округлена
+SELECT Cod, Price, FLOOR(Price * 30.3) AS USD, TRUNCATE(Price * 36.93, 0) AS UAH_without_cents, ROUND(Price * 36.93) AS Rounded_UAH FROM lab1.work;
+
+-- 16. Додати інформацію про нову книгу (всі колонки)
+INSERT INTO lab1.work (Numer, Cod, Name, Producer, Sides, Format, Date, Circulation, Topic, Category, Price, New) 
+VALUES (301, 8888, 'Інша нова книга', 'Інший Видавець', 250, '70х100/16', '2024-05-21', 500, 'Інша тема', 'Інша категорія', 0, 'No');
+
+-- 17. Додати інформацію про нову книгу (колонки обов'язкові для введення)
+INSERT INTO lab1.work (Cod, Name, Date) VALUES (9999, 'Нова книга', '2024-05-21');
+
+-- 18. Видалити книги, видані до 1990 року
+DELETE FROM lab1.work WHERE YEAR(Date) < 1990;
+
+-- 19. Проставити поточну дату для тих книг, у яких дата видання відсутня
+UPDATE lab1.work SET Date = CURRENT_DATE WHERE Date IS NULL;
+
+-- 20. Установити ознаку новинка для книг виданих після 2005 року
+UPDATE lab1.work SET New = 'Yes' WHERE YEAR(Date) >= 2005;
